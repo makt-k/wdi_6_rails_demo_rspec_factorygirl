39f6b91869bb16754aba91cf39d78ab1bf0e18f2
K Mak <karen.mak@stx.oxon.org>
2014-02-11 09:49:53 -0500

    Added spec for sign up error

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 feature 'Visitor signs up' do
-  scenario 'successfully' do
+  background do
     visit root_path
     click_link 'Sign Up'
+  end
+
+  scenario 'successfully' do
     fill_in 'Email', with: 'bob@example.com'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
@@ -12,5 +15,11 @@ feature 'Visitor signs up' do
     expect(page).to have_content 'Welcome!'
     expect(page).to have_content 'Signed in as bob@example.com'
     expect(page).to have_link 'Sign out'
+  end
+
+  scenario 'unsuccessfully with blank fields' do
+    click_button 'Sign up'
+    expect(page).to_not have_content('Signed in')
+    expect(page).to have_content("can't be blank")
   end
 end
