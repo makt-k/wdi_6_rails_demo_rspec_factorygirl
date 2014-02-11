922870a5b9c55396b94b7898d04b6ee0c7491218
Alex Grant <grantovich@users.noreply.github.com>
2014-02-10 21:51:48 -0500

    rails new ContactManager -T --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactManager::Application.config.secret_key_base = 'e0331047bc96389423f845e569283e18ff635b144dff7c30757e2926f3f1db2f2b1be7075276d6658ded8c07512792137528392c53fabb7a6f7a299ddc1593a6'
